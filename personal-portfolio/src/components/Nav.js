4575d03df82bdee2547e34ff1963d82710d3d67f
Grace Chung <hjngy0511@gmail.com>
2020-08-01 01:14:11 +0900

    Nav 컴포넌트의 이미지와 글귀 바뀌는 작업 완료 setInterval을 통해 자동으로 바뀌게하고싶었지만 거의 3시간을 해도 안됨. 그래서 일단 onclick이벤트로 앞 뒤롤 볼 수 있게 했다. 그러나 꼭 자동변경을 성공하고 싶다

--- personal-portfolio/src/components/Nav.js
+++ personal-portfolio/src/components/Nav.js
@@ -1,17 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Linkbar from './Linkbar'
 import { Carousel } from 'react-responsive-carousel'
+import { navData } from '../navData'
 // import {projectData} from '../projectData'
 
-class Nav extends Component {
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currPage: navData[0],
+    }
+    console.log('currPage: ', this.state.currPage)
+    this.i = 0
+  }
+
+  changeNextPage() {
+    setInterval(this.setState({ currPage: navData[this.i++] }), 100)
+    if (this.i >= navData.length) {
+      this.i = 0
+    }
+  }
+  changePrePage() {
+    setInterval(this.setState({ currPage: navData[this.i--] }), 100)
+    if (this.i <= 0) {
+      this.i = 0
+    }
+  }
+
   render() {
     return (
       <nav className="navbar">
-        <ul className="mySlides">
-          <li className="brainSlide"></li>
-          <li className="programmingSlide"></li>
-          <li className="dataSlide"></li>
-        </ul>
+        <div className="slide-container">
+          <ul className="mySlides">
+            <li className={this.state.currPage.About.imageName}>
+              <h1 className="introduction"> {this.state.currPage.About.intro}</h1>
+            </li>
+          </ul>
+          <a
+            href="#"
+            id="prev"
+            onClick={() => {
+              console.log('onLoad')
+              this.changePrePage()
+            }}
+          ></a>
+          <a
+            href="#"
+            id="next"
+            onClick={() => {
+              console.log('onLoad')
+              this.changeNextPage()
+            }}
+          ></a>
+        </div>
 
         <div className="navtitle">
           <div className="maintitle">Hyeonjeong Portfolio</div>
---
+++ personal-portfolio/src/navData.js
@@ -0,0 +1,29 @@
+export const navData = [
+  {
+    id: {
+      navId: '000001',
+    },
+    About: {
+      imageName: 'brainSlide',
+      intro: 'AI-driven Biomedical Imaging',
+    },
+  },
+  {
+    id: {
+      navId: '000002',
+    },
+    About: {
+      imageName: 'programmingSlide',
+      intro: 'Learning Harder & Work Smarter',
+    },
+  },
+  {
+    id: {
+      navId: '000003',
+    },
+    About: {
+      imageName: 'dataSlide',
+      intro: 'Data Driven Person',
+    },
+  },
+]

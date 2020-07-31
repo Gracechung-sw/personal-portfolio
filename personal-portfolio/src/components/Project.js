f5c0dbc578227b0292893f661256a7c6a70d29f1
Grace Chung <hjngy0511@gmail.com>
2020-07-31 20:37:54 +0900

    project 컴포넌트에 각 프로젝트 주제를 나타내는 메뉴선택창 추가 css 작업 완료 이제 state를 추가해서 해당 프로젝트 메뉴를 클릭하면 그에 맞는 프로젝트를 보여주게 만들 것이다.

--- personal-portfolio/src/components/Project.js
+++ personal-portfolio/src/components/Project.js
@@ -3,17 +3,27 @@ import { projectData } from '../projectData'
 
 const Project = () => (
   <div className="project_compo">
-    {projectData.map((project) => (
-      <div className="each-project">
-        <img className="project-image" src={project.snippet.thumbnails.default.url} />
-        <div className="project-item">
-          <div className="project-item-title">{project.snippet.title}</div>
-          <div className="project-item-desc">{project.snippet.description}</div>
+    {/* <h3 className="Project-header">PROJECT</h3> */}
+    <nav className="projectbar">
+      <ul className="projectbar-menu">
+        <li className="project-menu">Deep Learning</li>
+        <li className="project-menu">Front-end</li>
+        <li className="project-menu">Back-end</li>
+        <li className="project-menu">Brain-Cognitive Science</li>
+      </ul>
+    </nav>
+    <nav className="projectcontent">
+      {projectData.map((project) => (
+        <div className="each-project">
+          <img className="project-image" src={project.snippet.thumbnails.default.url} />
+          <div className="project-item">
+            <div className="project-item-title">{project.snippet.title}</div>
+            <div className="project-item-desc">{project.snippet.description}</div>
+          </div>
+          <a href={project.snippet.thumbnails.default.post} target="_new"></a>
         </div>
-        <a href={project.snippet.thumbnails.default.post} target="_new"></a>
-      </div>
-    ))}
-    <div> This is Project</div>
+      ))}
+    </nav>
   </div>
 )
 
